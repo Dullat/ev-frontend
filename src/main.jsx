4aca976b665193498cfe0c8d264bc1c2e9d2377d
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-07 20:33:37 +0530

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import RootLayout from "./layout/RootLayout.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<App />} />
+    </Route>,
+  ),
+);
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
