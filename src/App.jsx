1807e09e773aca4dd9fe0ec09a0dbaa9234490bd
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-07 20:22:40 +0530

    project setup: deps, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="bg-blue-600">Hello world</p>
+    </>
+  );
+}
+
+export default App;
