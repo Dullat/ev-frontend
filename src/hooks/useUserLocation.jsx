baca2e75631b7373165c9d8c4c76d01698578ea1
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-09 20:45:25 +0530

    make a hook to get user's location

---
+++ src/hooks/useUserLocation.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const useUserLocation = () => {
+  const [position, setPosition] = React.useState(null);
+  const [isWatching, setIsWatching] = React.useState(false);
+  const watchRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!navigator.geolocation) {
+      console.warn("Geolocation is not supported");
+      return;
+    }
+
+    watchRef.current = navigator.geolocation.watchPosition(
+      (pos) => {
+        const { latitude, longitude, heading } = pos.coords;
+        setPosition({ lat: latitude, lon: longitude, heading });
+        console.log(pos);
+      },
+      (err) => console.log("Geolocation error:", err),
+      {
+        enableHighAccuracy: true,
+        maximumAge: 5000,
+        timeout: 10000,
+      },
+    );
+
+    setIsWatching(true);
+
+    return () => {
+      if (watchRef.current) navigator.geolocation.clearWatch(watchRef.current);
+    };
+  }, []);
+
+  return { position, isWatching };
+};
+
+export default useUserLocation;
