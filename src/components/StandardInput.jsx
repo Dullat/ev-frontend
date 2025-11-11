22c9e829ea4cdf0c8ba1db2c3680e592e0fd0e8b
dullat <142017979+Dullat@users.noreply.github.com>
2025-11-12 00:36:47 +0530

    add : inavlid, validate fun, and validation callback

--- src/components/StandardInput.jsx
+++ src/components/StandardInput.jsx
@@ -1,4 +1,43 @@
-const StandardInput = ({ Icon, label, name, type = "text", placeholder }) => {
+import React from "react";
+
+const StandardInput = ({
+  Icon,
+  label,
+  name,
+  type = "text",
+  placeholder,
+  isEmpty,
+  validate,
+  onValidateChange,
+}) => {
+  const [hasValue, setHasValue] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const [empty, setEmpty] = React.useState(false);
+
+  const handleChange = (e) => {
+    const value = e.target.value.trim();
+    setHasValue(value !== "");
+
+    if (validate) {
+      const result = validate(value);
+
+      if (result === true) {
+        setError(null);
+        onValidateChange?.(name, true);
+      } else {
+        setError(result || "invalid input");
+        onValidateChange?.(name, false);
+      }
+    } else {
+      onValidateChange?.(name, value !== "");
+    }
+  };
+
+  React.useEffect(() => {
+    isEmpty && setEmpty(true);
+    hasValue && setEmpty(false);
+  }, [isEmpty, hasValue]);
+
   return (
     <div className={`flex flex-col gap-1 w-full`}>
       <label htmlFor="state" className="label ">
@@ -12,7 +51,14 @@ const StandardInput = ({ Icon, label, name, type = "text", placeholder }) => {
           type={type}
           name={name}
           placeholder={placeholder}
-          className={`w-full input pl-12 focus:border-blue-500 focus:ring-blue-100`}
+          onChange={handleChange}
+          className={`w-full input pl-12 focus:border-blue-500 focus:ring-blue-100 shadow-lg shadow-blue-200 ${
+            error || empty
+              ? "border-red-500 ring-4 ring-red-100 focus:border-red-500"
+              : hasValue
+                ? "border-blue-500 ring-4 ring-blue-100"
+                : "shadow-sm shadow-gray-100"
+          }`}
         />
       </div>
     </div>
