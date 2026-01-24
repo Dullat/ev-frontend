e8cb29e5d27d263dc29a5f86c2f12af33e4d720a
dullat <142017979+Dullat@users.noreply.github.com>
2026-01-24 13:23:13 +0530

    click outside

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,15 @@ const Search = () => {
     useLazySearchStationByQueryQuery();
   const [showList, setShowList] = React.useState(false);
   const timeoutRef = React.useRef(null);
+  const containerRef = React.useRef(null);
 
   const handleSearch = (e) => {
     clearTimeout(timeoutRef.current);
 
-    if (e.target.value.length < 2) return;
+    if (e.target.value.length < 2) {
+      setShowList(false);
+      return;
+    }
 
     timeoutRef.current = setTimeout(async () => {
       try {
@@ -31,8 +35,22 @@ const Search = () => {
   React.useEffect(() => {
     return clearTimeout(timeoutRef.current);
   }, []);
+
+  React.useEffect(() => {
+    function handleClickOutside(e) {
+      containerRef.current &&
+        !containerRef.current.contains(e.target) &&
+        setShowList(false);
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
   return (
-    <div className="w-full relative max-w-2xl rounded-full py-2 px-4 bg-blue-50 overflow-show">
+    <div
+      className="w-full relative max-w-2xl rounded-full py-2 px-4 bg-blue-50 overflow-show"
+      ref={containerRef}
+    >
       <div className="flex pl-8 relative">
         <SearchIcon className="absolute left-0 top-1/2 -translate-y-1/2 text-slate-800" />
         <input
@@ -41,10 +59,11 @@ const Search = () => {
           placeholder="Search stations or places"
           className={`text-slate-800 outline-none border-0 w-full`}
           onChange={(e) => handleSearch(e)}
+          onFocus={() => setShowList(true)}
         />
       </div>
 
-      {showList && data?.stations && (
+      {showList && data?.stations.length > 0 && (
         <div className="w-full absolute top-[60px] left-0 flex bg-white p-1 rounded-xl gap-1">
           <ul>
             {data.stations.map((station) => (
@@ -52,6 +71,7 @@ const Search = () => {
                 key={station._id}
                 to={`/${station.location.coordinates[1]}/${station.location.coordinates[0]}`}
                 className="py-2 px-4 relative pl-12 text-slate-800 flex items-center"
+                onClick={() => setShowList(false)}
               >
                 <EvCharger className="absolute top-1/2 left-3 -translate-y-1/2" />
                 {station.name}
