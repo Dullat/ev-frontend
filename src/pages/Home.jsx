2bcb434ac3835d244a8ac17245fbcdd3341b54c4
dullat <142017979+Dullat@users.noreply.github.com>
2026-01-24 20:34:13 +0530

    hero layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const Home = () => {
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex w-full items-stretch lg:items-start lg:pt-20 ">
+        <div className="flex w-full gap-2 flex-col lg:flex-row">
+          <div className="flex-1  aspect-[4/3] bg-slate-600">hahah</div>
+          <div className="flex-1 bg-slate-800">hahah</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
